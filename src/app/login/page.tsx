6370518540f6dc16a02e0097dce2efa7570f1c34
AviralChawla20 <aviralchawla02@gmail.com.com>
2024-01-13 10:24:47 +0530

    removed mistakes

---
+++ src/app/login/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+
+const App = () => (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+        <div className="max-w-md w-full p-6 bg-white rounded-md shadow-md">
+            <h2 className="text-3xl font-semibold text-center mb-6">Login</h2>
+            <Auth
+                supabaseClient={supabase}
+                appearance={{ theme: ThemeSupa }}
+                providers={['google', 'facebook', 'twitter']}
+            // className="space-y-4"
+            />
+        </div>
+    </div>
+);
+
+export default App;
